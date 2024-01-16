29f595d532c516c46df451b0de3c554e988412f7
[lococo0425] <[lococo0425@naver.com]>
2024-01-16 18:44:43 +0900

    Changes

---
+++ src/과제/과제1/과제5/Member.java
@@ -0,0 +1,20 @@
+package 과제.과제1.과제5;
+
+
+public class Member {
+    String id;
+    String pw;
+    String name;
+    String num;
+    String age;
+
+    public Member(String id, String pw, String name, String num, String age) {
+        this.id=id;
+        this.pw=pw;
+        this.name=name;
+        this.num=num;
+        this.age=age;
+
+
+    }
+}
